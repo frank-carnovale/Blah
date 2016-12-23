bb70ecac2e74255fb30ef93b2b03953963d1b1bf
Frank Carnovale <frank.carnovale@gmail.com>
2016-12-23 10:19:45 +0000

    more glossary entries; more tests (not working)

--- lib/BlahDB.pm
+++ lib/BlahDB.pm
@@ -6,7 +6,10 @@ sub connect_info { [ 'dbi:SQLite:t/example.db', '', ''] }
 
 sub glossary { +{
     cd => 'Compact Disk',
-    artist => 'Musician'
+    cdid => 'Compact Disk ID',
+    artistid => 'Musician ID',
+    artist => 'Musician',
+    year => 'Year Released'
 } }
 
 1;
